1f5472180a7a6d97356bd0f797ffffe048b50487
724888 <362294931@qq.com>
2017-03-15 20:06:59 +0800

    测试1

---
+++ src/longStr.java
@@ -0,0 +1,33 @@
+/**
+ * Created by 周杰伦 on 2017/3/15.
+ */
+
+
+import org.junit.Test;
+
+/**
+ * Created by 周杰伦 on 2017/3/15.
+ */
+public class longStr {
+    int findlongstr(char []str){
+        int max=1;
+        int j;
+
+        for(int i=0;i<str.length;i++){
+            int len=1;
+            j=i+1;
+            while(j<str.length && str[j]!=str[j-1]  ){len++;j++;}
+            if(len>max)max=len;
+            i=j-1;
+
+        }
+        return max;
+    }
+    @Test
+    public void test(){
+        char []str={'a','b','c'};
+        System.out.println(findlongstr(str));
+
+    }
+}
+
